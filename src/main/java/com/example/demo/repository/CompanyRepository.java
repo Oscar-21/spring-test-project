be8bb5e0c8099dd37f4d211da21c231b4b91bf32
Austin Conder <austin.conder@ruralsource.com>
2018-07-25 18:03:09 -0400

    clean

--- src/main/java/com/example/demo/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.domain.Company;
-
-public interface CompanyRepository extends CrudRepository<Company, Long> {
-
-}
